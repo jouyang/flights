0162d69b1e0145c4e89d1f52fc81f8ed4f9d02eb
Jeffrey Ouyang <jeffreyouyang5293@gmail.com>
2016-08-14 22:18:45 -0700

    testing changes

--- build/app.js
+++ build/app.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World\n');
+	res.end('Hello World, Welcome to flights\n');
 }).listen(8080, '107.170.230.178');
 console.log('Server running at http://107.170.230.178:8080/');
